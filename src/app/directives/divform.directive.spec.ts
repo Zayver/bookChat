dfc78c5273d234f87b09822407fc10440eb221fd
Zayver <zayber.re@gmail.com>
2024-10-07 12:23:51 -0500

    fix colors and added little anim

---
+++ src/app/directives/divform.directive.spec.ts
@@ -0,0 +1,8 @@
+import { DivformDirective } from './divform.directive';
+
+describe('DivformDirective', () => {
+  it('should create an instance', () => {
+    const directive = new DivformDirective();
+    expect(directive).toBeTruthy();
+  });
+});
